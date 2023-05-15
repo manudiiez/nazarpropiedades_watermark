149d05dd23482c015109f6d135377ad667fcf247
manolo2829 <manudiez2003@gmail.com>
2023-05-14 21:54:24 -0300

    click switch

--- src/js/script.js
+++ src/js/script.js
@@ -43,6 +43,7 @@ fileInput.addEventListener("change", async (e) => {
 // })
 
 downloadButton.addEventListener('click', async () => {
+    console.log('click');
     downloadButton.textContent = 'clickeado'
 
     if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) || window.innerWidth <= 550) {
